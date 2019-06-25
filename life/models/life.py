830994991f3f8177e2a43e39acd0aaefbe2443c4
jobiols <jorge.obiols@gmail.com>
2019-06-25 11:10:11 -0300

    [FIX] life

--- life/models/life.py
+++ life/models/life.py
@@ -3,6 +3,11 @@
 from odoo import api, fields, models
 import datetime as dt
 
+#este es el code que aparece en ntsw
+#M1805047502205
+
+# password cript for admin id = 28
+# $pbkdf2-sha512$25000$Y8yZ8z7HeC8F4HzvXWstRQ$oTBGJDQRNyqwXw0rAMnQk1vjJeEwvJFrhOD/fD2TNosISgMDz2f1JSXTXo/1fPeP/yMF.B2MHWll4O26X7CQ7w
 
 class Life(models.TransientModel):
     _name = 'lifes'
@@ -20,6 +25,6 @@ class Life(models.TransientModel):
         config.set_param('database.expiration_reason', 'trail')
 
         # actualizar fecha
-        date = dt.datetime.now() + dt.timedelta(days=32)
+        date = dt.datetime.now() + dt.timedelta(days=28)
         date = date.strftime('%Y-%m-%d 00:00:00')
         config.set_param('database.expiration_date', date)
